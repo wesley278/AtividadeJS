0bba8925a7b6424bfb853f5132fe8e03dc0ca71e
Wesley Souza <124937770+wesley278@users.noreply.github.com>
2023-05-31 23:07:07 -0300

    Add files via upload

---
+++ Atividade.Loops/js/exer11.js
@@ -0,0 +1,17 @@
+let numero1 = parseInt(prompt("Digite o primeiro número:"));
+let numero2 = parseInt(prompt("Digite o segundo número:"));
+let soma=0;
+
+if (numero1 > numero2) {
+  for (numero2; numero2<=numero1; numero2++) {
+    document.write(" "+numero2);
+    soma += numero2;
+  }
+} else if (numero2 > numero1) {
+  for (numero1; numero1<=numero2; numero1++) {
+    document.write(" "+numero1);
+    soma += numero1;
+  }
+}
+
+document.write("<br/>A soma desses números é "+soma);
